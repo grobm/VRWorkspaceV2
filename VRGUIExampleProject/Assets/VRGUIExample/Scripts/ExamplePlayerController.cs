648b8b4ff5cbc73d8436752c426ae771da575e27
Mark Grob <me@markgrob.com>
2015-05-10 21:29:03 -0400

    Rough beta... working web content workspace. You can have numerous webpages/apps in the workspace.

---
+++ VRGUIExampleProject/Assets/VRGUIExample/Scripts/ExamplePlayerController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExamplePlayerController : MonoBehaviour {
+	
+	public float moveAccelleration = 4f;
+	public float moveDrag = 2f;
+	public float mouseSensitivity = 2f;
+	
+	private float   rotationY = 0f;
+	private Vector3 velocity  = Vector3.zero;
+	private CharacterController controller;
+	public ExampleMenu menu;
+	
+	void Start()
+	{
+		controller = GetComponent<CharacterController>();
+		menu = GetComponent<ExampleMenu>();
+		
+		// hide the cursor
+		Cursor.visible = false;
+	}
+	
+	void FixedUpdate()
+	{
+		// toggle the menu on/off
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			menu.enabled = !menu.enabled;
+		}
+		
+		// disable movement when menu is open
+		if (!menu.enabled)
+		{
+			// very simplified movement
+			rotationY += Input.GetAxis("Mouse X") * mouseSensitivity;
+			transform.rotation = Quaternion.Euler(0f, rotationY, 0f);
+			
+			Vector3 moveKeys = new Vector3(
+				(Input.GetKey(KeyCode.A) ? -1 : 0) + (Input.GetKey(KeyCode.D) ? 1 : 0),
+				0,
+				(Input.GetKey(KeyCode.W) ? 1 : 0) + (Input.GetKey(KeyCode.S) ? -1 : 0));
+			
+			velocity += transform.rotation * moveKeys * moveAccelleration * Time.deltaTime;
+			velocity += -velocity.normalized * moveDrag * Time.deltaTime;
+			
+			controller.Move(velocity * Time.deltaTime);
+		}
+	}
+}
---
+++ VRGUIExampleProject/Assets/VRGUIExample/Scripts/VRWorkspaceMainMenu.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VRWorkspaceMainMenu : VRGUI {
+
+	public GUISkin skin;
+	public bool enabled;
+	public GameObject Workspace;
+	public int menumode;
+	public int sky;
+
+	public override void OnVRGUI()
+	{
+		if (enabled == true) {
+			GUI.skin = skin;
+			if (menumode== 0){
+				if (GUI.Button (new Rect (Screen.width / 2 - 100, 300, 320, 64), "Create VRworkspace")) {
+					print ("We have instance of VR Workspace");
+					Instantiate(Workspace, transform.position, transform.rotation);
+				}
+				if (GUI.Button (new Rect (Screen.width / 2 - 100, 400, 320, 64), "Settings")) {
+					menumode = 1;
+				}
+			}
+			if (menumode== 1){
+				if (GUI.Button (new Rect (Screen.width / 2 - 100, 300, 320, 64), "Select World")) {
+					sky = sky++;
+
+				}
+				if (GUI.Button (new Rect (Screen.width / 2 - 100, 400, 320, 64), "Workspace Mode")) {
+					menumode = 0;
+				}
+			}
+		}
+	}
+
+	void FixedUpdate()
+	{
+//		if (Input.GetKeyDown(KeyCode.Q))
+//			YRotation -= RotationRatchet;
+//
+		if (Input.GetKey ("`")){
+		    if (enabled == true){
+		    	enabled = false;
+				return;
+			}
+			if (enabled == false){
+		    	enabled = true;
+				return;
+			}
+		}
+	}
+}
